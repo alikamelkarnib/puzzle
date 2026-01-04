750a46750895d3ddcd5578d7c5ddfbb38dedc3bd
== <=>
2026-01-04 15:23:16 +0200

    first commit

---
+++ Assets/Common/Scripts/Dialog/ShopDialog.cs
@@ -0,0 +1,22 @@
+﻿ using UnityEngine.UI;
+
+public class ShopDialog : Dialog
+{
+    public Text[] valueTexts, priceTexts;
+    public Text watchAdValueText;
+
+    protected override void Start()
+    {
+        base.Start();
+ 
+
+        watchAdValueText.text = ConfigController.Config.rewardedVideoAmount + " Free";
+    }
+
+    public void OnBuyProduct(int index)
+	{
+		Sound.instance.PlayButton();
+     }
+
+    
+}
